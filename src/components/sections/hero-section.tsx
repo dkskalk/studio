f84859285d5c14eebba099e9e408b1810cbf118b
Kalk Douglas <dkskalk@gmail.com>
2025-10-30 03:30:50 +0000

    bloquei a funcionalidade de avançar o video, deixe apenas o botao nativo

--- src/components/sections/hero-section.tsx
+++ src/components/sections/hero-section.tsx
@@ -1,5 +1,6 @@
 import { Button } from '@/components/ui/button';
 import { Moon, Star } from 'lucide-react';
+import CustomVideoPlayer from '../ui/custom-video-player';
 
 export default function HeroSection() {
   return (
@@ -26,16 +27,7 @@ export default function HeroSection() {
         </h1>
         
         <div className="mt-10 max-w-3xl mx-auto">
-             <div className="relative aspect-video w-full rounded-lg overflow-hidden shadow-2xl border-4 border-background/50 bg-muted group">
-                <iframe 
-                    className="absolute inset-0 w-full h-full" 
-                    src="https://www.youtube.com/embed/FNymauRq1AA" 
-                    title="YouTube video player" 
-                    frameBorder="0" 
-                    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" 
-                    allowFullScreen>
-                </iframe>
-            </div>
+             <CustomVideoPlayer videoId="FNymauRq1AA" />
             <p className="mt-6 text-lg text-foreground/90">Assista a curta apresentação e descubra o segredo que centenas de mães estão usando para fazer seus filhos dormirem rápido.</p>
         </div>
 
