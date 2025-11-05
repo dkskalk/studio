73797fc4cbca8b01e0ec0b7a8a026af6d30de641
Kalk Douglas <dkskalk@gmail.com>
2025-11-05 02:39:50 +0000

    diminua o espaço livre no inicio dessa sessão, tem um espaço vazio muito

--- src/components/sections/offer-section.tsx
+++ src/components/sections/offer-section.tsx
@@ -139,7 +139,7 @@ export default function OfferSection() {
 
   return (
     <>
-      <section id="oferta" className="py-20 md:py-28 bg-background">
+      <section id="oferta" className="py-10 md:py-14 bg-background">
         <div className="container mx-auto px-4 md:px-6">
           <div className="max-w-4xl mx-auto text-center">
               <h2 className="font-headline text-3xl md:text-4xl font-bold text-foreground">
